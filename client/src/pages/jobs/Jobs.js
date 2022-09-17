8f9307cff13cc0079d61e9b4b3917b7e88d585fc
superneko <shilongfoo@gmail.com>
2022-09-17 19:03:00 +0800

    jobs page mvp done

--- client/src/pages/jobs/Jobs.js
+++ client/src/pages/jobs/Jobs.js
@@ -8,90 +8,79 @@ import Button from 'react-bootstrap/Button'
 import styles from './Jobs.module.scss'
 import 'react-datepicker/dist/react-datepicker.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPenToSquare, faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faBuilding } from '@fortawesome/free-regular-svg-icons'
+import { faCheck, faBriefcase, faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 import { faWhatsapp, faLinkedin } from '@fortawesome/free-brands-svg-icons'
-
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
-import { query, getFirestore, onSnapshot, collection } from 'firebase/firestore';
+import { getFirestore, collection, doc, setDoc, getDocs } from 'firebase/firestore';
 
 
 const Jobs = () => {
-    const [inputs, setInputs] = useState({
-        name: 'Matt Foo',
-        age: '22',
-        dob: '01/01/1980',
-        jobTitle: 'Designer',
-        company: 'Designer Pte Ltd',
-        companyLogo: '',
-        jobDescription: 'Help to create mockups and design dashboard',
-        startDate: '01/02/2022',
-        endDate: '01/07/2022'
-    })
-    const { name, age, dob, jobTitle, company, companyLogo, jobDescription, startDate, endDate } = inputs
     const db = getFirestore();
+    const [jobs, setJobs] = useState([])
+    const [job, setJob] = useState({})
 
-    // avatar
-    const [avatar, setAvatar] = useState('')
+    //event handlers
+    const addJob = async () => {
+        await setDoc(doc(db, 'jobs', 'jobs-5'), {
+            jobTitle: 'Senior frontend developer',
+            companyName: 'Fiverr',
+            location: 'Australia (remote)',
+            isRecruiting: 'Actively recruiting',
+            type: 'Full-time',
+            noOfEmployees: '10000-50000',
+            jobDescription: 'You will join our team and you’ll be responsible for co-creating interactive computer vision and other various application on the web. We highly value innovation – and you will think along with us about our current business processes and implementations.',
+            skills: {
+                1: 'You have a bachelor’s or master’s degree in IT or science – or you have a similar degree',
+                2: 'You have a significant amount of knowledge of both front and back-end developments (React, Bootstrap, Typescript, JavaScript, Node.js, SQL, NoSQL, Docker)',
+                3: 'You have knowledge of Database implementations such as SQL or NoSQL'
+            },
+            tasks: {
+                1: 'You will develop and implement various micro-services needed to visualize solar panel layouts with associated data such as wind, snow load, ballast, wiring scheme’s, etc',
+                2: 'You will improve both performance and features of existing micro-services',
+                3: 'You are responsible for the correct implementation of the design and also code yourself'
+            },
+            companyDescription: 'This company develops advanced WEB and CAD applications for solar energy systems, such as our renowned plugin for AutoCAD / BricsCAD (BIM).',
+            industry: 'Finance'
+        });
+        console.log('added job')
+    }
 
     // on load
     useEffect(() => {
-        const q = query(collection(db, 'myJobPortal'));
-        const unsub = onSnapshot(q, { includeMetadataChanges: false }, (snapshot) => {
-            const source = snapshot.metadata.fromCache ? 'local cache' : 'server';
-            snapshot.docChanges().forEach((change) => {
-                if (change.type === 'added') {
-                    const data = change.doc.data()
-                    setInputs({
-                        name: data.name,
-                        age: data.age,
-                        dob: data.dob,
-                        jobTitle: data.jobTitle,
-                        company: data.company,
-                        companyLogo: data.companyLogo,
-                        jobDescription: data.jobDescription,
-                        startDate: data.startDate,
-                        endDate: data.endDate
-                    })
-                    setAvatar(data.avatar)
-                }
-            })
-        })
-        return () => {
-            unsub();
+        async function fetchData() {
+            const querySnapshot = await getDocs(collection(db, 'jobs'));
+            const arr = []
+            querySnapshot.forEach((doc) => {
+                const data = doc.data()
+                data.id = doc.id.slice(-1)
+                arr.push(data)
+            });
+            setJobs(arr)
+            setJob(arr[0])
         }
+        fetchData()
     }, [])
 
 
     return (
         <Container>
+            {/* <Button onClick={() => addJob()}>Add job</Button> */}
             <Row>
-                <Col className='pe-0'>
+                <Col className='pe-sm-0'>
                     <div className={styles.list__white}>
-                        {[
-                            { name: 'John Lee', jobTitle: 'Developer at Google' },
-                            { name: 'Susan Koh', jobTitle: 'Designer at Mavrick' },
-                            { name: 'Shakesphere Tan', jobTitle: 'Poet lecturer at Singapore Polytechnic' },
-                            { name: 'Hafiz', jobTitle: 'Engineer at NTU' },
-                            { name: 'Lambert Lahm', jobTitle: 'QA Engineer at PlatsOrg' },
-                            { name: 'Lambert Lahm', jobTitle: 'QA Engineer at PlatsOrg' },
-                            { name: 'Lambert Lahm', jobTitle: 'QA Engineer at PlatsOrg' },
-                            { name: 'Lambert Lahm', jobTitle: 'QA Engineer at PlatsOrg' },
-                            { name: 'Lambert Lahm', jobTitle: 'QA Engineer at PlatsOrg' },
-                            { name: 'Lambert Lahm', jobTitle: 'QA Engineer at PlatsOrg' },
-                            { name: 'Lambert Lahm', jobTitle: 'QA Engineer at PlatsOrg' }
-
-                        ].map((ele, i) => {
+                        {jobs.map((ele, i) => {
                             return (
-                                <Row className='d-flex justify-content-between mt-3 border-bottom' key={i}>
-                                    <Col xs={3}>
-                                        <Image src='company1.jpg' alt='default-avatar' style={{ objectFit: 'cover', width: '70px', height: '70px' }} />
+                                <Row className={styles.row_clickable} key={i} onClick={() => setJob(ele)}>
+                                    <Col xs={4} sm={3}>
+                                        <Image src={`company${i}.jpg`} alt='company-logo' style={{ objectFit: 'cover', width: '70px', height: '70px' }} />
                                     </Col>
                                     <Col>
-                                        <p className='mb-0'>{ele.name}</p>
-                                        <small className='d-block text-secondary'>{ele.jobTitle}</small>
-                                        <Button variant="outline-secondary" className='mt-1 mb-3'>
-                                            <FontAwesomeIcon icon={faPlus} size='xs' className='me-2' />Add</Button>
+                                        <h6>{ele.jobTitle}</h6>
+                                        <p className='mb-0'>{ele.companyName}</p>
+                                        <small className='d-block'>{ele.location}</small>
+                                        <small className='d-block mb-2'> <FontAwesomeIcon icon={faCheck} className='me-1' color='green' />{ele.isRecruiting}</small>
                                     </Col>
                                 </Row>
                             )
@@ -99,60 +88,7 @@ const Jobs = () => {
                     </div>
                 </Col>
                 <Col sm={8} >
-                    <div className={styles.list__white}>
-                        <Row className='d-flex justify-content-between'>
-                            <Col xs={{ order: 2, span: 12 }} sm={{ order: 1, span: 6 }}>
-                                {avatar ?
-                                    <Image
-                                        roundedCircle
-                                        src={avatar}
-                                        width='107'
-                                        height='107'
-                                        alt=''
-                                        style={{ objectFit: 'cover' }}
-                                    />
-                                    : <Image src='profile-placeholder.png' alt='default-avatar' style={{ objectFit: 'cover', width: '107px', height: '107px' }} />}
-                            </Col>
-                            <Col xs={{ order: 1, span: 12 }} sm={{ order: 2, span: 6 }}>
-                                <Link to='/profile-settings'>
-                                    <FontAwesomeIcon icon={faPenToSquare} size='xl' className='float-end text-black' />
-                                </Link>
-
-                            </Col>
-                        </Row>
-
-
-
-                        <div className='d-flex mb-0'>
-                            <h3>{name}</h3>
-                            {age && <p className={styles.card__age}>{inputs.age}</p>}
-                        </div>
-                        <p className='mb-0'>Hi I&apos;m a full stack developer and I have built dashboards with React, Typescript, Bootstrap on the frontend and Nodejs, SQL, Graphql for the backend</p>
-                        <small>Singapore</small>
-
-                        <p className='mb-0 mt-2'>
-                            <FontAwesomeIcon icon={faEnvelope} size='xl' className='me-2' />shilongfoo@gmail.com
-                        </p>
-                        <p className='mb-0'>
-                            <FontAwesomeIcon icon={faWhatsapp} size='xl' className='me-2' />+65 83687202 (preferred)
-                        </p>
-                        <a className='mb-3 text-black' href='https://www.linkedin.com/in/shilong-foo-a8b721144/'>
-                            <FontAwesomeIcon icon={faLinkedin} size='xl' className='me-2' />https://www.linkedin.com/in/shilong-foo-a8b721144/
-                        </a>
-                        <h3>Career</h3>
-                        {jobTitle && <h6 className='mb-0'>{jobTitle}</h6>}
-                        {company && <p className='mb-0'>{company}                         {startDate && <small className='mb-3 d-block'>{startDate} - {endDate ? endDate : 'Present'}</small>}
-                        </p>}
-                        {jobDescription &&
-                            <div className='mb-3'>
-                                <h6 className='mb-0'>Job Description:</h6>
-                                <small >{jobDescription}</small>
-                            </div>
-                        }
-
-
-                    </div>
-
+                    <EachJob job={job} />
                 </Col>
             </Row>
 
@@ -161,4 +97,63 @@ const Jobs = () => {
     )
 }
 
+const EachJob = ({ job }) => {
+    return (
+        <>
+            {Object.keys(job).length !== 0 &&
+                <div className={styles.list__white}>
+                    <h3 className='mt-3'>{job?.jobTitle}</h3>
+                    <Row sm={4} className='gx-0'>
+                        <Col sm={4}>{job?.companyName},&nbsp; {job?.location}</Col>
+                        <Col sm={2}>3 days ago</Col>
+                        <Col>Over 100 applicants</Col>
+                    </Row>
+                    <p className='mt-3'>
+                        <FontAwesomeIcon icon={faBriefcase} size='xl' className='me-2' />{job?.type}
+                    </p>
+                    <p>
+                        <FontAwesomeIcon icon={faBuilding} size='xl' className='me-2' />{job?.noOfEmployees} employees
+                    </p>
+                    <Button variant="primary" className='text-white mb-3'>
+                        <FontAwesomeIcon icon={faArrowUpRightFromSquare} size='lg' className='me-2' />Apply
+                    </Button>
+                    <h6>Job Description</h6>
+                    <p>{job?.jobDescription}</p>
+                    <h6>What skills and experience you will need</h6>
+                    <ul>
+                        <li>{job?.skills[1]}</li>
+                        <li>{job?.skills[2]}</li>
+                        <li>{job?.skills[3]}</li>
+                    </ul>
+                    <h6>Tasks</h6>
+                    <ul>
+                        <li>{job?.tasks[1]}</li>
+                        <li>{job?.tasks[2]}</li>
+                        <li>{job?.tasks[3]}</li>
+                    </ul>
+                    <Card className='mt-5 p-1 p-sm-1'>
+                        <Card.Body>
+                            <h4>About the company</h4>
+                            <Row className='mt-3 mb-3'>
+                                <Col xs={3} sm={1} className='me-4'>
+                                    <Image src={`company${job.id}.jpg`} alt='company-logo' style={{ objectFit: 'cover', width: '70px', height: '70px' }} />
+                                </Col>
+                                <Col>
+                                    <h5 className='pt-2'>{job.companyName}</h5>
+                                    <p>3000 followers</p>
+                                </Col>
+                            </Row>
+                            <p>{job.industry}, &nbsp; {job.noOfEmployees} employees</p>
+                            <p>{job.companyDescription}</p>
+                        </Card.Body>
+                    </Card>
+                </div >
+            }
+        </>
+
+    )
+}
+EachJob.propTypes = {
+    job: PropTypes.object.isRequired
+}
 export default Jobs
