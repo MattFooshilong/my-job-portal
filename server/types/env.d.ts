fd281f55d593f119bd60639956fc0d7c97eae5c9
Matt Foo <shilongfoo@gmail.com>
2025-04-25 21:45:00 +0800

    updated vercel.json

--- server/types/env.d.ts
+++ server/types/env.d.ts
@@ -1,16 +1,16 @@
 export {}
 import express from "express"
 
-//declare global {
-//  namespace NodeJS {
-//    interface ProcessEnv {
-//      ACCESS_TOKEN_SECRET: string
-//      REFRESH_TOKEN_SECRET: string
-//      NODE_ENV: string
-//      ENV: "test" | "dev" | "prod"
-//    }
-//  }
-//}
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      ACCESS_TOKEN_SECRET: string
+      REFRESH_TOKEN_SECRET: string
+      NODE_ENV: string
+      ENV: "test" | "dev" | "prod"
+    }
+  }
+}
 //declare module "express-serve-static-core" {
 //  interface Request {
 //    csrfToken?: string
