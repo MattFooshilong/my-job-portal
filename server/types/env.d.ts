102c4b345b97af42d372c2ca9ef5c2550d6ffd5b
Matt Foo <shilongfoo@gmail.com>
2025-04-25 22:42:26 +0800

    env.d.ts change

--- server/types/env.d.ts
+++ server/types/env.d.ts
@@ -1,5 +1,14 @@
 import express from "express"
-
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      ACCESS_TOKEN_SECRET: string
+      REFRESH_TOKEN_SECRET: string
+      NODE_ENV: string
+      ENV: "test" | "dev" | "prod"
+    }
+  }
+}
 declare global {
   namespace Express {
     interface Request {
