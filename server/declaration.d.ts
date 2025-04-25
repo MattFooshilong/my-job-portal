f0572653dbb0bdd80a9e4931f1241c7eb98ffa06
Matt Foo <shilongfoo@gmail.com>
2025-04-25 20:15:11 +0800

    added declaration.d.ts to server for secret keys

---
+++ server/declaration.d.ts
@@ -0,0 +1,12 @@
+export {}
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      ACCESS_TOKEN_SECRET: string
+      REFRESH_TOKEN_SECRET: string
+      NODE_ENV: string
+      ENV: "test" | "dev" | "prod"
+    }
+  }
+}
