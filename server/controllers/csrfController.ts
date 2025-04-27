cac502bab1c74702f883fec90310a474ebd2f390
Matt Foo <shilongfoo@gmail.com>
2025-04-27 16:31:48 +0800

    changed csrfCookie sameSite to none for testing

--- server/controllers/csrfController.ts
+++ server/controllers/csrfController.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express"
 const generateCSRFToken = (req: Request, res: Response, next: NextFunction): void => {
   const csrfToken = crypto.randomUUID()
   req.csrfToken = csrfToken
-  res.cookie("cookieCsrfToken", csrfToken, { httpOnly: true, secure: true, sameSite: "strict", maxAge: 1 * 60 * 60 * 1000 })
+  res.cookie("cookieCsrfToken", csrfToken, { httpOnly: true, secure: true, sameSite: "none", maxAge: 1 * 60 * 60 * 1000 })
   next()
 }
 const validateCSRFToken = (req: Request, res: Response, next: NextFunction) => {
