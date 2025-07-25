5c086a376b6dd20e0d303109130c180f817da132
Matt Foo <shilongfoo@gmail.com>
2025-07-25 19:45:09 +0800

    fixed some server side errors

--- server/declaration.d.ts
+++ server/declaration.d.ts
@@ -1,12 +1,19 @@
-export {}
+export {};
 
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      ACCESS_TOKEN_SECRET: string
-      REFRESH_TOKEN_SECRET: string
-      NODE_ENV: string
-      ENV: "test" | "dev" | "prod"
+      ACCESS_TOKEN_SECRET: string;
+      REFRESH_TOKEN_SECRET: string;
+      NODE_ENV: string;
+      ENV: "test" | "dev" | "prod";
+    }
+  }
+  namespace Express {
+    interface Request {
+      csrfToken?: string;
+      email?: string;
+      roles?: number[];
     }
   }
 }
