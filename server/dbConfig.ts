44a448b253b148f817f3379feb4b248318b08bc6
Matt Foo <shilongfoo@gmail.com>
2025-07-30 20:19:55 +0800

    removed accessing db using pg because it uses admin role which does not respect RLS and permissions such as grant and revoke

--- server/dbConfig.ts
+++
@@ -1,7 +0,0 @@
-import { Pool, Client } from "pg";
-const connectionString = process.env.DATABASE_URL;
-const client = new Client({
-  connectionString
-});
-await client.connect().then(() => console.log("postgres db connected"));
-export default client;
