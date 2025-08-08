a331b62bf3079cd70fb87ca5c46b0d2685bd41b2
Matt Foo <shilongfoo@gmail.com>
2025-08-08 20:43:43 +0800

    refactored adminDashboard to use tanstack query

--- client/src/pages/admin-dashboard/AdminDashboard.tsx
+++ client/src/pages/admin-dashboard/AdminDashboard.tsx
@@ -16,7 +16,7 @@ import { faBriefcase, faClose, faList, faSearch } from "@fortawesome/free-solid-
 import { useState, useEffect } from "react";
 import useAxiosWithInterceptors from "../../hooks/useAxiosWithInterceptors";
 import useLogout from "../../hooks/useLogout";
-import { useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 type JobStatus = "Successful" | "Unsuccessful" | "InProgress";
 
@@ -30,16 +30,16 @@ type Application = {
 };
 
 const AdminDashboard = () => {
-  const [applications, setApplications] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [updatingJob, setUpdatingJob] = useState(false);
-  const [filteredData, setFilteredData] = useState([]);
+  const [filteredData, setFilteredData] = useState<Application[]>([]);
   const [filteredStatus, setFilteredStatus] = useState("");
+  const [selectedId, setSelectedId] = useState(0);
   const logout = useLogout();
   const axiosPrivate = useAxiosWithInterceptors();
   const queryClient = useQueryClient();
 
-  const updateJobStatus = async (application: Application, status: string) => {
+  const updateJobStatus = async (application: Application, status: string, selectedId: number) => {
+    setSelectedId(selectedId);
     setUpdatingJob(true);
     const dataObject = {
       status
@@ -49,6 +49,7 @@ const AdminDashboard = () => {
       const statusUpdated = response?.data?.statusUpdated; //do smth later perhaps?
       setUpdatingJob(false);
       queryClient.invalidateQueries({ queryKey: ["getJobApplications"] });
+      queryClient.invalidateQueries({ queryKey: ["getJobsApplicationsDashboard"] });
     } catch (error) {
       setUpdatingJob(false);
       console.log(error);
@@ -57,165 +58,181 @@ const AdminDashboard = () => {
 
   const filterStatus = (status: string) => {
     if (status === "") {
-      setFilteredData(applications);
+      setFilteredData(applications ?? []);
       setFilteredStatus("");
     } else {
-      const filtered = applications.filter((ele: Application) => ele.status === status);
-      setFilteredData(filtered);
+      const filtered = applications?.filter((ele: Application) => ele.status === status);
+      setFilteredData(filtered ?? []);
       setFilteredStatus(status);
     }
   };
   // search by applicant name, company name or job title
   const filterBySearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     const text = e.target.value.toLowerCase();
-    const filtered = applications.filter((ele: Application) => ele.user_name.toLowerCase().includes(text) || ele.company_name.toLowerCase().includes(text) || ele.job_title.toLowerCase().includes(text));
-    setFilteredData(filtered);
+    const filtered = applications?.filter((ele: Application) => ele.user_name.toLowerCase().includes(text) || ele.company_name.toLowerCase().includes(text) || ele.job_title.toLowerCase().includes(text));
+    setFilteredData(filtered ?? []);
   };
-  useEffect(() => {
-    const fetchData = async () => {
+  const getJobsApplicationsDashboard = async () => {
+    try {
+      const res = await axiosPrivate.get("/get-jobs-where-there-is-application");
+      return res.data;
+    } catch (err) {
+      console.error(err);
       try {
-        setLoading(true);
-        const response = await axiosPrivate.get("/get-jobs-where-there-is-application"); //protected route, will throw an error if refreshToken is expired
-        console.log(response);
-        setApplications(response.data);
-        setFilteredData(response.data); // none filtered at first
-        setLoading(false);
-      } catch (error) {
-        setLoading(false);
-        try {
-          await logout(); // Will throw if logout fails
-        } catch (logoutError) {
-          console.error("Error during logout:", logoutError);
-          // Handle logout-specific errors here
-        }
+        await logout();
+      } catch (err) {
+        console.error(err);
       }
-    };
-    fetchData();
-  }, [updatingJob]);
+    }
+  };
+  const {
+    isPending,
+    isError,
+    data: applications,
+    error
+  } = useQuery<Application[]>({
+    queryKey: ["getJobsApplicationsDashboard"],
+    queryFn: getJobsApplicationsDashboard,
+    staleTime: 3 * 24 * 60 * 60 //cacheTime 3 days
+  });
+
+  useEffect(() => {
+    if (applications) {
+      setFilteredData(applications); // none filtered at first
+    }
+  }, [applications]);
+
+  if (isPending) {
+    return <Spinner animation="border" className="mt-5" />;
+  }
+
+  if (isError) {
+    return <span>Error: {error?.message}</span>;
+  }
   return (
     <Container>
-      {loading ? (
-        <Spinner animation="border" className="mt-5" />
-      ) : (
-        <>
-          <Row className="mt-4">
-            <Col>
-              <h3>Job Applications</h3>
-            </Col>
-          </Row>
-          <Row className="mb-4">
-            <Col lg={6}>
-              <div className={styles.customCard}>
-                <Row className="mb-4">
-                  <Col>
-                    <h4>Applications</h4>
-                  </Col>
-                </Row>
-                <Row>
-                  <Col className="pe-0">
-                    <p className={styles.status}>In Progress</p>
-                    <div className={styles.statusCount}>{applications.filter((ele: Application) => ele.status === "InProgress").length}</div>
-                  </Col>
-                  <Col className="pe-0">
-                    <p className={styles.status}>Successful</p>
-                    <div className={styles.statusCount}>{applications.filter((ele: Application) => ele.status === "Successful").length}</div>
-                  </Col>
-                  <Col className="pe-0">
-                    <p className={styles.status}>Unsuccessful</p>
-                    <div className={styles.statusCountFontSizeOnly}>{applications.filter((ele: Application) => ele.status === "Unsuccessful").length}</div>
-                  </Col>
-                </Row>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col lg={6}>
-              <Stack direction="horizontal" gap={3}>
-                <InputGroup>
-                  <InputGroup.Text id="searchText">
-                    <FontAwesomeIcon icon={faSearch} className="me-1" />
-                  </InputGroup.Text>
-                  <Form.Control placeholder="Search name /company name/ job title" aria-label="search" aria-describedby="basic-addon1" onChange={(e: React.ChangeEvent<HTMLInputElement>) => filterBySearch(e)} />
-                </InputGroup>
-                <Dropdown>
-                  <Dropdown.Toggle variant="secondary" id="status-filter" size="sm" className={styles.statusFilter}>
-                    Status
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => filterStatus("Successful")}>Successful</Dropdown.Item>
-                    <Dropdown.Item onClick={() => filterStatus("Unsuccessful")}>Unsuccessful</Dropdown.Item>
-                    <Dropdown.Item onClick={() => filterStatus("InProgress")}>In Progress</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-                {filteredStatus !== "" && (
-                  <Badge pill bg="secondary" onClick={() => filterStatus("")} style={{ cursor: "pointer" }}>
-                    <FontAwesomeIcon icon={faClose} className="me-1" />
-                    {filteredStatus}
-                  </Badge>
-                )}
-              </Stack>
-            </Col>
-          </Row>
+      <Row className="mt-4">
+        <Col>
+          <h3>Job Applications</h3>
+        </Col>
+      </Row>
+      <Row className="mb-4">
+        <Col lg={6}>
+          <div className={styles.customCard}>
+            <Row className="mb-4">
+              <Col>
+                <h4>Applications</h4>
+              </Col>
+            </Row>
+            <Row>
+              <Col className="pe-0">
+                <p className={styles.status}>In Progress</p>
+                <div className={styles.statusCount}>{applications?.filter((ele: Application) => ele.status === "InProgress").length}</div>
+              </Col>
+              <Col className="pe-0">
+                <p className={styles.status}>Successful</p>
+                <div className={styles.statusCount}>{applications?.filter((ele: Application) => ele.status === "Successful").length}</div>
+              </Col>
+              <Col className="pe-0">
+                <p className={styles.status}>Unsuccessful</p>
+                <div className={styles.statusCountFontSizeOnly}>{applications?.filter((ele: Application) => ele.status === "Unsuccessful").length}</div>
+              </Col>
+            </Row>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col lg={6}>
+          <Stack direction="horizontal" gap={3}>
+            <InputGroup>
+              <InputGroup.Text id="searchText">
+                <FontAwesomeIcon icon={faSearch} className="me-1" />
+              </InputGroup.Text>
+              <Form.Control placeholder="Search Name / Company name / Job title" aria-label="search" aria-describedby="basic-addon1" onChange={(e: React.ChangeEvent<HTMLInputElement>) => filterBySearch(e)} />
+            </InputGroup>
+            <Dropdown>
+              <Dropdown.Toggle variant="secondary" id="status-filter" size="sm" className={styles.statusFilter}>
+                Status
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item onClick={() => filterStatus("Successful")}>Successful</Dropdown.Item>
+                <Dropdown.Item onClick={() => filterStatus("Unsuccessful")}>Unsuccessful</Dropdown.Item>
+                <Dropdown.Item onClick={() => filterStatus("InProgress")}>In Progress</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+            {filteredStatus !== "" && (
+              <Badge pill bg="secondary" onClick={() => filterStatus("")} style={{ cursor: "pointer" }}>
+                <FontAwesomeIcon icon={faClose} className="me-1" />
+                {filteredStatus}
+              </Badge>
+            )}
+          </Stack>
+        </Col>
+      </Row>
 
-          <Table bordered responsive hover className={styles.table}>
-            <thead>
-              <tr>
-                <th>
-                  <FontAwesomeIcon icon={faCalendar} className="me-1" />
-                  Date
-                </th>
-                <th>
-                  <FontAwesomeIcon icon={faUser} className="me-1" />
-                  Name
-                </th>
-                <th>
-                  <FontAwesomeIcon icon={faBuilding} className="me-1" />
-                  Company Name
-                </th>
-                <th>
-                  <FontAwesomeIcon icon={faBriefcase} className="me-1" />
-                  Job Title
-                </th>
-                <th>
-                  <FontAwesomeIcon icon={faList} className="me-1" />
-                  Application Status
-                </th>
-                <th>
-                  <FontAwesomeIcon icon={faList} className="me-1" />
-                  Action
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredData.map((ele: Application, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{dayjs(ele?.user_jobs_created_at).format("DD/MM/YYYY")}</td>
-                    <td>{ele?.user_name}</td>
-                    <td>{ele?.company_name}</td>
-                    <td>{ele?.job_title}</td>
-                    <td>{ele.status === "InProgress" ? <Badge bg="secondary">In Progress</Badge> : ele.status === "Successful" ? <Badge bg="success">Successful</Badge> : <Badge bg="danger">Unsuccessful</Badge>}</td>
-                    <td>
-                      <Dropdown>
-                        <Dropdown.Toggle variant="success" id="dropdown-basic" size="sm">
-                          Select
-                        </Dropdown.Toggle>
-                        <Dropdown.Menu>
-                          <Dropdown.Item onClick={() => updateJobStatus(ele, "Successful")}>{updatingJob ? <Spinner animation="border" className="mt-5" /> : "Successful"}</Dropdown.Item>
-                          <Dropdown.Item onClick={() => updateJobStatus(ele, "Unsuccessful")}>{updatingJob ? <Spinner animation="border" className="mt-5" /> : "Unsuccessful"}</Dropdown.Item>
-                          <Dropdown.Item onClick={() => updateJobStatus(ele, "InProgress")}>{updatingJob ? <Spinner animation="border" className="mt-5" /> : "InProgress"}</Dropdown.Item>
-                        </Dropdown.Menu>
-                      </Dropdown>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </>
-      )}
+      <Table bordered responsive hover className={styles.table}>
+        <thead>
+          <tr>
+            <th>
+              <FontAwesomeIcon icon={faCalendar} className="me-1" />
+              Date
+            </th>
+            <th>
+              <FontAwesomeIcon icon={faUser} className="me-1" />
+              Name
+            </th>
+            <th>
+              <FontAwesomeIcon icon={faBuilding} className="me-1" />
+              Company Name
+            </th>
+            <th>
+              <FontAwesomeIcon icon={faBriefcase} className="me-1" />
+              Job Title
+            </th>
+            <th>
+              <FontAwesomeIcon icon={faList} className="me-1" />
+              Application Status
+            </th>
+            <th>
+              <FontAwesomeIcon icon={faList} className="me-1" />
+              Action
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {filteredData
+            ?.sort((a, b) => a.user_jobs_id - b.user_jobs_id)
+            .map((ele: Application, i) => {
+              return (
+                <tr key={i}>
+                  <td>{dayjs(ele?.user_jobs_created_at).format("DD/MM/YYYY")}</td>
+                  <td>{ele?.user_name}</td>
+                  <td>{ele?.company_name}</td>
+                  <td>{ele?.job_title}</td>
+                  <td>{updatingJob && ele.user_jobs_id === selectedId ? <Spinner animation="border" size="sm" /> : <StatusBadge status={ele.status} />}</td>
+                  <td>
+                    <Dropdown>
+                      <Dropdown.Toggle variant="success" id="dropdown-basic" size="sm">
+                        Select
+                      </Dropdown.Toggle>
+                      <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => updateJobStatus(ele, "Successful", ele.user_jobs_id)}>Successful</Dropdown.Item>
+                        <Dropdown.Item onClick={() => updateJobStatus(ele, "Unsuccessful", ele.user_jobs_id)}>Unsuccessful</Dropdown.Item>
+                        <Dropdown.Item onClick={() => updateJobStatus(ele, "InProgress", ele.user_jobs_id)}>InProgress</Dropdown.Item>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </Table>
     </Container>
   );
+};
+
+const StatusBadge = ({ status }: { status: JobStatus }) => {
+  return status === "InProgress" ? <Badge bg="secondary">In Progress</Badge> : status === "Successful" ? <Badge bg="success">Successful</Badge> : <Badge bg="danger">Unsuccessful</Badge>;
 };
 
 export default AdminDashboard;
